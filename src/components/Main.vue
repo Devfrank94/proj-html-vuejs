55cc90940af1791e0ed12451db2aa4b4951ec187
Devfrank94 <francescomurro94@gmail.com>
2023-05-10 14:52:07 +0200

    completed cards in section 1

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -16,16 +16,47 @@ export default {
 
   <!-- *****2.1 - Section 1 Meet New Agency***** -->
 
-    <section class="Sect1">
-      <div class="fmcontainer test">
-        <div class="title-cont d-flex flex-dir-col al-item-cent">
-          <h3 class="mt-5 mb-4">meet the new agency SEO template<br>from the avada team</h3>
-          <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br> Dolor, nam voluptas dolorum placeat libero itaque?</p>
-        </div>
-
+  <section class="Sect1">
+    <div class="container">
+      <div class="title-cont d-flex flex-dir-col al-item-cent">
+        <h3 class="mt-5 mb-4">meet the new agency SEO template<br>from the avada team</h3>
+        <p class="mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br> Dolor, nam voluptas dolorum placeat libero itaque?</p>
       </div>
 
-    </section>
+        <!-- ****CARDS**** -->
+      <div class="row row-cols-3">
+        <div class="cont-card">
+          <div class="fmcard py-5 px-5 bg-white">
+            <i class="fa-solid fa-road fa-xl" style="color: #ffa737;"></i>
+            <div class="title-cont d-flex flex-dir-col al-item-cent">
+              <h4 class="mt-3 mb-2">establish goals</h4>
+              <p class="mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, nam voluptas dolorum placeat libero</p>
+            </div>
+          </div>
+        </div>
+        <div class="cont-card">
+          <div class="fmcard py-5 px-5 bg-white">
+            <i class="fa-solid fa-video fa-xl" style="color: #ffa737;"></i>
+            <div class="title-cont d-flex flex-dir-col al-item-cent">
+              <h4 class="mt-3 mb-2">work with a team</h4>
+              <p class="mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, nam voluptas dolorum placeat libero</p>
+            </div>
+          </div>
+        </div>
+        <div class="cont-card">
+          <div class="fmcard py-5 px-5 bg-white">
+            <i class="fa-solid fa-droplet fa-xl" style="color: #ffa737;"></i>
+            <div class="title-cont d-flex flex-dir-col al-item-cent">
+              <h4 class="mt-3 mb-2">get results</h4>
+              <p class="mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, nam voluptas dolorum placeat libero</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- ****/CARDS**** -->
+
+  </section>
 
 
 
@@ -55,8 +86,7 @@ export default {
   // 2. Main
   // - 2.1 - Section 1 Meet New Agency
   section:nth-child(odd){
-    background-color: $bg-sect;
-    height: 400px; //da rimuovere
+    background-color: $bg-sect;  
   }
 
   .title-cont{
@@ -66,10 +96,24 @@ export default {
     font-weight: 700;
     }
 
-    p{
-      font-size: .8rem;
-      color: $gray;
+    h4{
+    font-size: 1rem;
+    font-weight: 800;
     }
+
+    p{
+    font-size: .8rem;
+    color: $gray;
+    }
+ 
   }
 
+  .fmcard{
+    border-radius: 5px;
+
+    p{
+    font-size: .7rem;
+    }
+  }
+  
 </style>
